29ff0476d9c5df63f85542d33687daedfc07c793
ekllerr <eklerzzy@gmail.com>
2025-04-29 12:47:49 +0200

    basic ui added

---
+++ src/components/GameContainer/GameContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/gameContainer.css';
+
+const GameContainer = () => {
+    return (
+        <div className="game-container">
+
+        </div>
+    );
+};
+
+export default GameContainer;
